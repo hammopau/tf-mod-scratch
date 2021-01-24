297cfa2497b9644d2c3f5ec0decbbe747a124e2a
Paul Hammond <paul.hammond@zoyoglobal.com>
2021-01-24 16:51:59 +0000

    stuff

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "example" {
+  value = var.example
+}
+
+output "platform" {
+  value = var.platform
+}
+
+output "module_name" {
+  value = var.module_name
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "example" {
+  default = "not_set"
+}
+
+variable "platform" {
+  default = "not_set"
+}
+
+variable "module_name" {
+  default = "not_set"
+}
