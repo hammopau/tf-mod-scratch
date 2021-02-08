54dfbd6b62ba156f79fba27392e31b0602a38730
Paul Hammond <paul.hammond@zoyoglobal.com>
2021-02-08 19:38:58 +0000

    stuff

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "aws_account_id" {
+  value = var.aws_account_id
+}
+
+output "aws_account_name" {
+  value = var.aws_account_name
+}
+
 output "example" {
   value = var.example
 }
@@ -8,4 +16,8 @@ output "platform" {
 
 output "module_name" {
   value = var.module_name
+}
+
+output "tf_version" {
+  value = var.tf_version
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "aws_account_id" {
+  default = "not_set"
+}
+
+variable "aws_account_name" {
+  default = "not_set"
+}
+
 variable "example" {
   default = "not_set"
 }
@@ -7,5 +15,13 @@ variable "platform" {
 }
 
 variable "module_name" {
+  default = "not_set"
+}
+
+variable "input_json_file" {
+  default = "not_set"
+}
+
+variable "tf_version" {
   default = "not_set"
 }
